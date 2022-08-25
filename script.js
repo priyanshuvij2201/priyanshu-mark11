6c869e3124bac761842f72d1fb0e04694d2a25ad
priyanshuvij2201 <PRIYANSHUVIJ456@GMAIL.COM>
2022-08-26 01:51:33 +0530

    Update
    Updates JS

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const dateOfBirth=document.querySelector("#date-of-birth");
 const luckyNumber=document.querySelector("#lucky-number");
-const checkNumber=document.querySelector("#check-number");
-checkNumberButton.addEventListener('click',function 
-getValues(){
-    console.log(dateOfBirth.value,luckyNumber.value);
-    
-})
+const checkNumberButton=document.querySelector("#check-number");
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum  = calculateSum(dob);
+    console.log(sum);
+}
+function calculateSum(dob)
+{
+    dob = dob.replaceAll("-","");
+    console.log(dob);
+    let sum=0;
+    for(let index=0;index<dob.length;index++)
+    {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
