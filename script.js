f88f74b662671b0798d729d6b3c41788b751b9dc
priyanshuvij2201 <PRIYANSHUVIJ456@GMAIL.COM>
2022-08-26 19:46:30 +0530

    Updated all CSS, JS

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const dateOfBirth=document.querySelector("#date-of-birth");
 const luckyNumber=document.querySelector("#lucky-number");
 const checkNumberButton=document.querySelector("#check-number");
+const outputBoxArea= document.querySelector("#output-boxarea");
+//window.confirm("This is to inform you that this website does not store any data.Please press OK to confirm");
 
 function checkBirthDateIsLucky(){
     const dob = dateOfBirth.value;
     const sum  = calculateSum(dob);
-    console.log(sum);
+    const lucky= luckyNumber.value;
+    if(sum>0 && lucky>0)
+    compareValues(sum,luckyNumber.value);
+    else
+    outputBoxArea.innerText="Please enter both the values";
 }
+function compareValues(sum,luckyNumber)
+{
+    if(sum%luckyNumber==0)
+    { outputBoxArea.innerText ="Your birthday is a lucky number ";
+   
+}else
+    outputBoxArea.innerText ="Your birthday is not a lucky number ";
+
+}
+function bothValues(){}
+
 function calculateSum(dob)
 {
     dob = dob.replaceAll("-","");
-    console.log(dob);
+  
     let sum=0;
     for(let index=0;index<dob.length;index++)
     {
